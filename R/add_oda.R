8c50cd7af8f0315d170fd3e4a8b625c22740dc27
milesdwilliams15 <milesdwilliams15@gmail.com>
2025-09-14 20:28:18 -0400

    pusing the beta version

--- R/add_oda.R
+++ R/add_oda.R
@@ -3,10 +3,29 @@
 #' This function takes either a dyad-year or state-year dataset made
 #' with the `{peacesciencer}` R package and populates it with official
 #' development assistance (ODA) comments and disbursements from the 
-#' Organization for Economic Cooperation and Development (OECD) --- tables 
+#' Organization for Economic Cooperation and Development (OECD), specifically the data tables 
 #' DAC2a (disbursements) and DAC3a (commitments). Values are in millions of
 #' 2023 U.S. dollars.
 #' 
+#' @param data A data frame created by one of the `create_*()` functions in the `{peacesciencer}` R package.
+#' @param type A character string indicating either `"donor"` or `"recipient"`. Relevant only if the data object is a state-year level data frame. The value `"donor"` (default) means the data will be populated with ODA values given by donor countries. The value `"recipient"` means the data will be populated with ODA received by countries.
+#' @return `add_oda()` returns either a state-year or dyad-year data frame populated with bilateral official development assistance (ODA) commitments and disbursements in millions of 2023 US Dollars. All zero values are true zeros and all `NA` values are truly invalid. ODA data runs from 1960 to 2023 for disbursements and from 1966 to 2023 for commitments. If the supplied data object is a dyad-year data frame, ODA values are totals for a donor-recipient pair. If the supplied data is a state-year data frame, ODA values are total bilateral ODA for either a donor (if `type = "donor"`) or a recipient (if `type = "recipient"`).
+#' @description The `add_oda()` function works by reading in ODA data from DAC2a and DAC3a data tables via a developer API from the OECD data explorer (https://data-explorer.oecd.org/). This approach has certain trade-offs. The main benefit is that the user does not have to eat up extra RAM storing a large-ish dyadic donor-recipient dataset in memory permanently and locally on your own machine. The downside is that to access the data in an active R session you need an internet connection, and calling the data via the OECD's API may take a few seconds to a minute depending on the quality of your connection. I think these costs are well worth the benefit of saving RAM.
+#' @examples 
+#' library(tidyverse)
+#' library(peacesciencer)
+#' library(odadata)
+#' 
+#' # make a dyad-year dataset:
+#' dt <- create_dyadyears(subset_years = 1960:2023) |>
+#'   add_oda() |>
+#'   drop_na(oda_committed) # drops invalid donor-recipient pairs
+#'
+#' # make a state-year dataset for aid repients:
+#' dt <- create_stateyears(subset_years = 1960:2023) |>
+#'   add_oda(type = "recipient") |>
+#'   drop_na(oda_committed) # drops non-recipients 
+#' @export
 add_oda <- function(data, type = "donor") {
   
   ## check that you have the right kind of dataset
@@ -18,7 +37,7 @@ add_oda <- function(data, type = "donor") {
   
   ## check which kind of dataset it is
   dyadic <- 
-    any(colnames(data) %in% c("ccode1", "gwcode1")) &
+    any(colnames(data) %in% c("ccode1", "gwcode1")) | 
     any(colnames(data) %in% c("ccode2", "gwcode2"))
   cow <-
     any(colnames(data) %in% c("ccode", "ccode1", "ccode2"))
@@ -134,25 +153,31 @@ add_oda <- function(data, type = "donor") {
         )
       )
   } else if(!dyadic & cow) {
-    if(type == "donor") {
-      c_names <- suppressWarnings(tibble(
-        DONOR = unique(dac_stacked$DONOR),
-        ccode = countrycode::countrycode(
-          DONOR, "iso3c", "cown"
-        )
-      ))
-    } else if(type == "recipient") {
-      c_names <- suppressWarnings(tibble(
-        RECIPIENT = unique(dac_stacked$RECIPIENT),
-        ccode = countrycode::countrycode(
-          RECIPIENT, "iso3c", "cown"
-        )
-      ))
-    }
+    donor_names <- suppressWarnings(tibble(
+      DONOR = unique(dac_stacked$DONOR),
+      ccode1 = countrycode::countrycode(
+        DONOR, "iso3c", "cown"
+      )
+    ))
+    recipient_names <- suppressWarnings(tibble(
+      RECIPIENT = unique(dac_stacked$RECIPIENT),
+      ccode2 = countrycode::countrycode(
+        RECIPIENT, "iso3c", "cown"
+      )
+    ))
   
     oda_data <- dac_stacked |>
-      left_join(c_names, by = str_to_upper(type)) |>
-      drop_na(ccode) |>
+      left_join(donor_names, by = "DONOR") |>
+      left_join(recipient_names, by = "RECIPIENT") |>
+      drop_na(ccode1, ccode2)
+    if(type == "donor") {
+      oda_data <- oda_data |>
+        rename(ccode = ccode1)
+    } else {
+      oda_data <- oda_data |>
+        rename(ccode = ccode2)
+    }
+    oda_data <- oda_data |>
       transmute(
         ccode, year = TIME_PERIOD,
         oda = OBS_VALUE,
@@ -175,38 +200,44 @@ add_oda <- function(data, type = "donor") {
       left_join(oda_data, by = c("ccode", "year")) |>
       mutate(
         oda_disbursed = ifelse(
-          ccode %in% c_names$ccode &
+          ccode %in% donor_names$ccode1 &
             year %in% 1960:2023,
           replace_na(oda_disbursed, 0),
           oda_disbursed
         ),
         oda_committed = ifelse(
-          gwcode %in% c_names$gwcode &
+          ccode %in% donor_names$ccode1 &
             year %in% 1966:2023,
           replace_na(oda_committed, 0),
           oda_committed
         )
       )
   } else {
-    if(type == "donor") {
-      c_names <- suppressWarnings(tibble(
-        DONOR = unique(dac_stacked$DONOR),
-        gwcode = countrycode::countrycode(
-          DONOR, "iso3c", "gwn"
-        )
-      ))
-    } else if(type == "recipient") {
-      c_names <- suppressWarnings(tibble(
-        RECIPIENT = unique(dac_stacked$RECIPIENT),
-        gwcode = countrycode::countrycode(
-          RECIPIENT, "iso3c", "gwn"
-        )
-      ))
-    }
+    donor_names <- suppressWarnings(tibble(
+      DONOR = unique(dac_stacked$DONOR),
+      gwcode1 = countrycode::countrycode(
+        DONOR, "iso3c", "gwn"
+      )
+    ))
+    recipient_names <- suppressWarnings(tibble(
+      RECIPIENT = unique(dac_stacked$RECIPIENT),
+      gwcode2 = countrycode::countrycode(
+        RECIPIENT, "iso3c", "gwn"
+      )
+    ))
     
     oda_data <- dac_stacked |>
-      left_join(c_names, by = str_to_upper(type)) |>
-      drop_na(gwcode) |>
+      left_join(donor_names, by = "DONOR") |>
+      left_join(recipient_names, by = "RECIPIENT") |>
+      drop_na(gwcode1, gwcode2)
+    if(type == "donor") {
+      oda_data <- oda_data |>
+        rename(gwcode = gwcode1)
+    } else {
+      oda_data <- oda_data |>
+        rename(gwcode = gwcode2)
+    }
+    oda_data <- oda_data |>
       transmute(
         gwcode, year = TIME_PERIOD,
         oda = OBS_VALUE,
@@ -229,13 +260,13 @@ add_oda <- function(data, type = "donor") {
       left_join(oda_data, by = c("gwcode", "year")) |>
       mutate(
         oda_disbursed = ifelse(
-          gwcode %in% c_names$gwcode &
+          gwcode %in% donor_names$gwcode1 &
             year %in% 1960:2023,
           replace_na(oda_disbursed, 0),
           oda_disbursed
         ),
         oda_committed = ifelse(
-          gwcode %in% c_names$gwcode &
+          gwcode %in% donor_names$gwcode1 &
             year %in% 1966:2023,
           replace_na(oda_committed, 0),
           oda_committed
@@ -248,13 +279,3 @@ add_oda <- function(data, type = "donor") {
   
 }
 
-
-## TEST 
-library(peacesciencer)
-library(tidyverse)
-
-# dyadic + cow
-dt <- create_stateyears(
-  system = "gw",
-  subset_years = 1960:1970
-) |> add_oda()
